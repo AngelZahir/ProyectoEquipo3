b3557d66f6b71dbbeae872885c0f41ecac424354
DavidMiller97 <david211910@gmail.com>
2021-06-12 12:43:55 -0500

    titulo

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Info 6</title>
+    <title>Proyecto</title>
 </head>
 <body>
     @include('partials.nav-header-main')
